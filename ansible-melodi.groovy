8e68f0b7e376484e629df939f2dbcb22bb1ac2d3
Adilet Abdraimov <saifullo.adi@gmail.com>
2020-08-28 01:20:30 +0000

    updated final version

--- ansible-melodi.groovy
+++ ansible-melodi.groovy
@@ -1,25 +1,23 @@
+  
 properties([
     parameters([
         string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true),
         string(defaultValue: '', description: 'Please enter branch name', name: 'branch', trim: true)
         ])
     ])
+
 if (nodeIP?.trim()) {
     node {
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            stage('Pull Repo') {
-                git branch: '${branch}', changelog: false, poll: false, url: 'https://github.com/ikambarov/ansible-melodi.git'
-            }
-            withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-                stage("Install Apache"){
-                    sh '''
-                        export ANSIBLE_HOST_KEY_CHECKING=False
-                        ansible-playbook -i "68.183.122.68," --private-key $SSHKEY main.yml
-                        '''
-                }
-            }
+        stage('Pull Repo') {
+            git branch: 'master', changelog: false, poll: false, url: 'https://github.com/ikambarov/ansible-melodi.git'
         }
-    }
+
+        withEnv(['ANSIBLE_HOST_KEY_CHECKING=False']) {
+            stage("Install Apache"){
+                ansiblePlaybook credentialsId: 'jenkins-master-ssh-key', extras: '-e melodi_branch=${branch}', inventory: '${nodeIP},', playbook: 'main.yml'
+                }
+            }  
+        }  
 }
 else {
     error 'Please enter valid IP address'
